adcddffc0f42e0b251e3b37e4c1834d5fb1f8181
markus8006 <guilhermemarkus8@gmail.com>
2025-12-08 20:50:48 -0300

    novas pages

--- src/app/routes/main_route.py
+++ src/app/routes/main_route.py
@@ -5,10 +5,23 @@ main = Blueprint('main', __name__)
 
 
 @main.route("/")
-def main_page():
+def home_page():
     return render_template("home.html")
 
 
 @main.route("/serviços")
-def servicos():
+def servicos_page():
     return render_template("servicos.html")
+
+@main.route("/contato")
+def contato_page():
+    return render_template("contato.html")
+
+@main.route("/depoimentos")
+def depoimentos_page():
+    return render_template("depoimentos.html")
+
+@main.route("/sobre")
+def sobre_page():
+    return render_template("sobre.html")
+
